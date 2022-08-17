08cd871726fde0e1b9d9cede055066ed5cec6f5e
hesung <hsjo6864@gmail.com>
2022-08-17 15:56:25 +0900

    2022.08.17 Spring DB - JDBC Study Project - JDBC 로 DB 단건 Select 로직 구현 및 테스트

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,6 +1,8 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,13 +11,23 @@ import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
 
     @Test
     void crud() throws SQLException {
+        //save
         Member member = new Member("v0", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+
+        log.info("same = {}", member == findMember);
+        log.info("equal = {}", member.equals(findMember) ); //@Data 에서 equals and hashcode 포함
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
